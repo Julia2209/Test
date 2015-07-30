23f68ffff9dd5307b88afceaafe8e81561804f90
Julia2209 <amrinlamrin21@gmail.com>
2015-07-30 08:08:30 +0000

    New rspec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -16,18 +16,6 @@ RSpec.describe ProductsController, type: :controller do
     it { should render_template :new }
   end
   
-  describe "destroy action" do
-    let(:product) { stub_model Product }
-    
-    before { expect(Product).to receive(:find).with('10').and_return(product) }
-    
-    before { expect(product).to receive(:destroy).and_return(true) }
-    
-    before { delete :destroy, id: 10 }
-    
-    it { should render_template :destroy }
-  end
-  
   describe "create action" do
     before { @product = stub_model Product }
     
@@ -38,9 +26,52 @@ RSpec.describe ProductsController, type: :controller do
       
       before { post :create, product: { name: "Beer", description: "Good" } }
       
-      it { should render_template :create }
+      it { should redirect_to(product_path(@product.id)) }
+    end
+    context do
+      before { expect(@product).to receive(:save).and_return(false) }
+      
+      before { post :create, product: { name: "Beer", description: "Good" } }
+      
+      it { should render_template :new }
     end
   end
+  
+  describe "update action" do
+    let(:product) { stub_model Product }
+
+    before { expect(Product).to receive(:find).with('12').and_return(product) }
+
+    context do
+      before { expect(product).to receive(:update).with('name' => 'Milk').and_return(true) }
+
+      before { patch :update, id: 12, product: { name: 'Milk' } }
+
+      it { should redirect_to(product_path(product)) }
+    end
+    
+    context do
+      before { expect(product).to receive(:update).with('name' => 'Milk').and_return(false) }
+      
+      before { patch :update, id: 12, product:{ name: 'Milk' } }
+      
+      it { should render_template :edit }
+    end
+  end
+  
+  describe "destroy action" do
+    let(:product) { stub_model Product }
+    
+    before { expect(Product).to receive(:find).with('10').and_return(product) }
+    
+    before { expect(product).to receive(:destroy).and_return(true) }
+    
+    before { delete :destroy, id: 10 }
+    
+    it { should redirect_to(products_url) }
+  end
+  
+  
 end
 
 
